c5ec9b5412f70133dc1b7d5bf65e62c65c4fb9fa
snsrrhc3078 <98512002+snsrrhc3078@users.noreply.github.com>
2022-12-19 17:02:20 +0900

    [feat] MyButton 템플릿 완성

--- src/com/dietdiary/components/MyButton.java
+++ src/com/dietdiary/components/MyButton.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
@@ -15,6 +17,8 @@ import javax.swing.plaf.BorderUIResource.LineBorderUIResource;
 public class MyButton extends JButton{
 	String label;
 	Dimension labelSize;
+	boolean isMouseOn;
+	boolean isMouseClicked;
 	public MyButton(String label) {
 		super(label);
 		this.label = label;
@@ -23,17 +27,42 @@ public class MyButton extends JButton{
 		labelSize = getPreferredSize();
 		setPreferredSize(d);
 		
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				isMouseOn = true;
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				isMouseOn = false;
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				isMouseClicked = true;
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				isMouseClicked = false;
+			}
+		});
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		g.setColor(new Color(224,184, 138));
 		g.fillRect(0, 0, getWidth(), getHeight());
-		g.setColor(new Color(188, 94, 0));
+		if(isMouseClicked) {
+			g.setColor(new Color(98, 52, 0));
+		}else {
+			g.setColor(new Color(188, 94, 0));
+		}
 		g.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
 		g.setColor(Color.BLACK);
 //		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
 		g.setFont(new Font(getFont().getFontName(), Font.BOLD, getFont().getSize()));
 		g.drawString(label, getWidth()/2-(labelSize.width/2), getHeight()/2+ 5);
+		if(isMouseOn) {
+			g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
+		}
 	}
 }
